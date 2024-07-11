1cfd073bee17c24bb3e32ed65e84fac5f3630eec
TheMikeKaisen <98148929+TheMikeKaisen@users.noreply.github.com>
2024-07-11 06:13:35 +0530

    create Board popover form

--- flowstate/hooks/use-action.ts
+++ flowstate/hooks/use-action.ts
@@ -15,42 +15,44 @@ export const useAction = <TInput, TOutput>(
   action: Action<TInput, TOutput>,
   options: UseActionOptions<TOutput> = {}
 ) => {
-    const [FieldErrors, setFieldErrors] = useState<FieldErrors<TInput> | undefined>(undefined)
-    const [error, setError] = useState<string | undefined>(undefined)
-    const [data, setData] = useState<TOutput | undefined>(undefined)
-    const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [FieldErrors, setFieldErrors] = useState<
+    FieldErrors<TInput> | undefined
+  >(undefined);
+  const [error, setError] = useState<string | undefined>(undefined);
+  const [data, setData] = useState<TOutput | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-    const execute = useCallback(
-        async(input: TInput) => {
-            setIsLoading(true);
-            try {
-                const result = await action(input);
-                if(!result){
-                    return
-                }
-                if(result.fieldErrors){
-                    setFieldErrors(result.fieldErrors)
-                }
-                if(result.error){
-                    setError(result.error)
-                    options.onError?.(result.error)
-                }
-                if(result.data){
-                    setData(result.data)
-                    options.onSuccess?.(result.data)
-                }
-            } finally {
-                setIsLoading(false);
-                options.onComplete?.()
-            }
-        }, [action, options]
-    )
+  const execute = useCallback(
+    async (input: TInput) => {
+      setIsLoading(true);
+      try {
+        const result = await action(input);
+        if (!result) {
+          return;
+        }
+        setFieldErrors(result.fieldErrors);
 
-    return{
-        execute, 
-        FieldErrors, 
-        error, 
-        data, 
-        isLoading
-    }
+        if (result.error) {
+          setError(result.error);
+          options.onError?.(result.error);
+        }
+        if (result.data) {
+          setData(result.data);
+          options.onSuccess?.(result.data);
+        }
+      } finally {
+        setIsLoading(false);
+        options.onComplete?.();
+      }
+    },
+    [action, options]
+  );
+
+  return {
+    execute,
+    FieldErrors,
+    error,
+    data,
+    isLoading,
+  };
 };
