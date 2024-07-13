30a694093c9cb2f65d1ff8a49d01a6a8461573cf
TheMikeKaisen <98148929+TheMikeKaisen@users.noreply.github.com>
2024-07-13 12:07:58 +0530

    create list component

--- flowstate/app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ flowstate/app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,8 +1,44 @@
+import { db } from "@/lib/db.connect";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import ListContainer from "./_components/list-container";
+import { List } from "@prisma/client";
 
-const BoardIdPage = () => {
+interface BoardIdPageProps {
+  params: {
+    boardId: string;
+  }
+}
+
+const BoardIdPage = async({params}: BoardIdPageProps) => {
+  const {orgId} = auth();
+  if(!orgId){
+    redirect('/select-org')
+  }
+  const lists = await db.list.findMany({
+    where: {
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: "asc",
+        },
+      },
+    },
+    orderBy: {
+      order: "asc",
+    },
+  });
   return (
-    <div className="h-full">
-      BoardId Page
+    <div className="h-full p-4 overflow-x-auto">
+      <ListContainer 
+        boardId={params.boardId}
+        data={lists}
+      />
+      
     </div>
   )
 }
