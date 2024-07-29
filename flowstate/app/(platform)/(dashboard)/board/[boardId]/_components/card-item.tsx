5b607a7c9df1e79ac708c11c6724df62f13d8b27
TheMikeKaisen <h.karthiknair@gmail.com>
2024-07-29 14:00:01 +0530

    create Card Modal

--- flowstate/app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ flowstate/app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useCardModal } from "@/hooks/use-card-modal";
 import { Card } from "@prisma/client";
 
 interface CardItemProps {
@@ -8,8 +9,10 @@ interface CardItemProps {
 }
 
 export const CardItem = ({data, index}:CardItemProps) => {
+
+    const cardModal = useCardModal();
     return (
-        <div className="truncate border-2 border-transparent hover:border-gray-200 hover:transition py-2 px-3 text-sm bg-white rounded-md shadow-sm" role="button">
+        <div className="truncate border-2 border-transparent hover:border-gray-200 hover:transition py-2 px-3 text-sm bg-white rounded-md shadow-sm" role="button" onClick={() => cardModal.onOpen(data.id)}>
             {data.title}
         </div>
     )
--- flowstate/app/(platform)/layout.tsx
+++ flowstate/app/(platform)/layout.tsx
@@ -1,11 +1,17 @@
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { QueryProvider } from "@/components/providers/queryProvider";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 
 const platformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
+      <QueryProvider>
+
       <Toaster />
+      <ModalProvider />
       {children}
+      </QueryProvider>
     </ClerkProvider>
   );
 };
